ac301da4ac1196bb23488034fc6ced0aec8efa69
Gülsüm Demirbaş <gdem2334@gmail.com>
2024-05-15 14:42:59 +0300

    Initial Commit

---
+++ WeatherProject/WeatherProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherProjectApp.swift
+//  WeatherProject
+//
+//  Created by Gülsüm Demirbaş on 15.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
